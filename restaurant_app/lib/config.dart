8576f72d297d1bf715a8a34638c3b2b468a9b8a8
dlsdk0601 <dlsdk0601@gmail.com>
2024-05-19 23:32:54 +0900

    feat: 서버 주소 및 logger level 추가

--- restaurant_app/lib/config.dart
+++ restaurant_app/lib/config.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:logger/logger.dart';
 
 final logger = Logger();
@@ -6,6 +7,10 @@ class Config {
   Config._();
 
   bool splashFadeIn = true;
+
+  Level loggerLevel = kDebugMode ? Level.debug : Level.warning;
+
+  late Uri apiServer = Uri.parse('http://localhost:5001/api');
 }
 
 final config = Config._();
